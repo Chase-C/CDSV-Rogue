85814bfc343ac971f758b0aa3b1d901d07dc4b19
Chase Cummings <chaseecummings@gmail.com>
2013-11-10 07:31:08 -0800

    added enemy movement

--- src/cdsv_rogue/spells/RockThrow.java
+++ src/cdsv_rogue/spells/RockThrow.java
@@ -10,13 +10,13 @@ import cdsv_rogue.Unit;
 public class RockThrow extends Spell {
 
 	public RockThrow(Room room, Entity parent, float x, float y, float tx, float ty, float dx, float dy) {
-		super("RockThrow", room, parent, x, y, tx, ty, dx, dy, 2.5f, Color.gray);
+		super("RockThrow", room, parent, x, y, tx, ty, dx / 1.5f, dy / 1.5f, 2.5f, Color.gray);
 		room.addSpell(this);
 	}
 
 	@Override
 	public void activate(Unit u) {
-		u.stun(15, 1500, 35);
+		u.stun(22, 1500, 35);
 	}
 
 }
