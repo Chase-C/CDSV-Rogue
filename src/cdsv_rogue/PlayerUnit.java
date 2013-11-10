cac2a2f09b334639dcf7265dc565dd9bd5722579
TheBlueGamer55 <endless.answers@gmail.com>
2013-11-09 20:34:40 -0800

    collision

--- src/cdsv_rogue/PlayerUnit.java
+++ src/cdsv_rogue/PlayerUnit.java
@@ -6,7 +6,7 @@ public class PlayerUnit extends Unit{
 	
 	public PlayerUnit(float x, float y) {
 		super(x, y);
-		setHitBox(0, 0, 32, 32);
+		setHitBox(0, 0, 16, 16);
 		currentSpell = 1; //sets the current spell to fireball
 		addType("PLAYER"); //to be used for collision
 		//control schemes can be referred
@@ -19,7 +19,7 @@ public class PlayerUnit extends Unit{
 	
 	public void render(GameContainer gc, Graphics g) throws SlickException{
 		g.setColor(Color.blue);
-		g.drawRect(x, y, 32, 32);
+		g.drawRect(x, y, 16, 16);
 	}
 	
 	public void update(GameContainer gc, int delta) throws SlickException{
@@ -30,7 +30,7 @@ public class PlayerUnit extends Unit{
 	public void move(){
 		//movement code moves the player if no Entities of type SOLID are in the way
 		if(check("RIGHT")){ 
-			if(collide(SOLID, x + 2, y) == null){ //collide() returns null if there's no collision
+			if(collide(SOLID, x + 2, y) == null){ 
 				x += 2;
 			}
 		}
--- src/cdsv_rogue/TestRoom.java
+++ src/cdsv_rogue/TestRoom.java
@@ -1,20 +1,31 @@
 package cdsv_rogue;
 
-import java.util.ArrayList;
-
 import it.randomtower.engine.World;
+import it.randomtower.engine.entity.Solid;
+
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
+import org.newdawn.slick.tiled.TiledMap;
 
 public class TestRoom extends World{
+	
+	private TiledMap room;
 
-	public TestRoom(int id, GameContainer gc) {
+	public TestRoom(int id, GameContainer gc) throws SlickException{
 		super(id, gc);
 	}
 	
 	//initializes anything right when the state starts
+	
 	public void init(GameContainer gc, StateBasedGame sbg) throws SlickException{
 		super.init(gc, sbg);
+		room = new TiledMap("res/levels/exampleRoom.tmx");
+		for(int i = 0; i < room.getObjectCount(0); i++){
+			Solid wall = new Solid(room.getObjectX(0, i), room.getObjectY(0, i),
+									room.getObjectWidth(0, i), room.getObjectHeight(0, i),
+									0, new Image(room.getObjectWidth(0, i), room.getObjectHeight(0, i)));
+			add(wall);
+		}
 		PlayerUnit player = new PlayerUnit(320, 240);
 		//the add() method adds any Entity to a list, where all of the rendering and updating happens
 		add(player); 
@@ -22,6 +33,7 @@ public class TestRoom extends World{
 	
 	//takes care of any of the rendering and graphics in the state
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g) throws SlickException{
+		room.render(0, 0);
 		super.render(gc, sbg, g);
 	}
 	
